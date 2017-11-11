32a280468ec5f836a1b949d10d35ff020e048d57
qzh <zhangq5721@gmail.com>
2017-11-11 13:28:07 +0100

    Add project files

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+// 应用目录为当前目录
+define('APP_PATH', __DIR__ . '/');
+if($_SERVER['SERVER_ADDR']=='127.0.0.1'){
+	define('PROJECT_DIR', '/sun-cashmere/');
+}
+else{
+	define('PROJECT_DIR', '/');
+}
+
+//echo 'APP_PATH: ' . APP_PATH . '<br />';
+// open debug mode
+define('APP_DEBUG', true);
+//echo $_SERVER['DOCUMENT_ROOT'];
+// load framework file 
+require(APP_PATH.'frameworkCore/FrameWorkCore.php');
+// load function library
+require(APP_PATH . 'frameworkCore/Common.php');
+
+// load configuration file
+$config = require(APP_PATH . 'config/config.php');
+
+// instantiate FrameworkCore class
+$fw = new FrameWorkCore($config);
+$fw->run();
+$dir = $_SERVER['REQUEST_URI'];
+
